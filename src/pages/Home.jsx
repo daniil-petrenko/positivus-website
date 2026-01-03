d12a382eacb95584b90b3acb756887f88b29b3aa
Daniil Petrenko <danya.petrenko1981@gmail.com>
2026-01-03 21:05:19 +0200

    Add the Case Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import MainLayout from '@/layouts/MainLayout/MainLayout';
 import Hero from '@components/Hero'
 import Services from '@/components/Services';
 import Proposal from '@/components/Proposal';
+import Case from '@/components/Case';
 
 const Home = () => {
    return (
@@ -11,6 +12,7 @@ const Home = () => {
          <Companies className="pt-8.75 pb-17.5 max-xs:py-7.5" />
          <Services className="pt-17.5 pb-15 max-xs:py-7.5" />
          <Proposal className="pt-15 pb-20 max-xs:py-7.5" />
+         <Case className="pt-20 pb-17.5 max-xs:py-7.5"/>
       </MainLayout>
    );
 };
